bbc0bacffc55feb1a2af6e39f40a16bbcda4d58d
plm9606 <plm9606@naver.com>
2020-06-03 03:12:39 +0900

    test: search서비스 스터디그룹 테스트코드 수정
    수정된 함수 파라미터 반영
    테스트 돌아가도록 수정
    studygroup 인덱스에 목데이터 삽입 후 테스트

--- server/services/search/test/group.test.js
+++ server/services/search/test/group.test.js
@@ -10,12 +10,12 @@ const {
 
 const testGroups = [
   {
-    _id: "1",
-    title: "test",
+    _id: "111",
+    title: "test_study",
     subtitle: "subtitle1",
     intro: "intro1",
     isRecruiting: true,
-    category: ["프로그래밍", "파이썬"],
+    category: ["프로그래밍", "파이썬", "test_group"],
     tags: ["알고리즘 ", "파이썬 ", "python ", "algorithm"],
     days: [0, 4],
     leader: "test1@gmail.com",
@@ -29,13 +29,13 @@ const testGroups = [
     members: []
   },
   {
-    _id: "2",
-    title: "test",
+    _id: "211",
+    title: "test_study",
     subtitle: "subtitle2",
     intro: "intro2",
     isRecruiting: true,
     category: ["프로그래밍", "자바"],
-    tags: ["알고리즘 ", "java ", "자바 ", "algorithm"],
+    tags: ["알고리즘 ", "java ", "자바 ", "algorithm", "test_study"],
     days: [0, 4],
     leader: "test1@gmail.com",
     startTime: 17,
@@ -50,24 +50,25 @@ const testGroups = [
 ];
 
 const ADD_testGroups = [
-  `{"_id":"1", "title": "test", "subtitle": "subtitle1", "intro" : "intro1", "isRecruiting" : true, "category" : [ "프로그래밍", "파이썬"] , "tags" : ["알고리즘 ","파이썬 ","python ","algorithm"],"days" : [0, 4], "leader" : "test1@gmail.com", "startTime" : 17, "min_personnel" : 4, "max_personnel" : 8, "location" : { "lat" : 41.12, "lon" : -50.34 },"endTime" : 19,"thumbnail" : "https://kr.object.ncloudstorage.com/studycombined/groupImage/1575602565910algorith.png","members" : [ ]}`,
-  `{"_id":"2", "title": "test", "subtitle": "subtitle2", "intro" : "intro2", "isRecruiting" : true, "category" : [ "프로그래밍", "자바"], "tags" : ["알고리즘 ","java ","자바 ","algorithm"],"days" : [0, 4], "leader" : "test1@gmail.com", "startTime" : 17, "min_personnel" : 4, "max_personnel" : 8, "location" : { "lat" : 41.12, "lon" : -50.34 },"endTime" : 19,"thumbnail" : "https://kr.object.ncloudstorage.com/studycombined/groupImage/1575602565910algorith.png","members" : [ ]}`
+  `{"_id":"111", "title": "test_study", "subtitle": "subtitle1", "intro" : "intro1", "isRecruiting" : true, "category" : [ "프로그래밍", "파이썬","test_group"] , "tags" : ["알고리즘 ","파이썬 ","python ","algorithm"],"days" : [0, 4], "leader" : "test1@gmail.com", "startTime" : 17, "min_personnel" : 4, "max_personnel" : 8, "location" : { "lat" : 41.12, "lon" : -50.34 },"endTime" : 19,"thumbnail" : "https://kr.object.ncloudstorage.com/studycombined/groupImage/1575602565910algorith.png","members" : [ ]}`,
+  `{"_id":"211", "title": "test_study", "subtitle": "subtitle2", "intro" : "intro2", "isRecruiting" : true, "category" : [ "프로그래밍", "자바"], "tags" : ["알고리즘 ","java ","자바 ","algorithm","test_study"],"days" : [0, 4], "leader" : "test1@gmail.com", "startTime" : 17, "min_personnel" : 4, "max_personnel" : 8, "location" : { "lat" : 41.12, "lon" : -50.34 },"endTime" : 19,"thumbnail" : "https://kr.object.ncloudstorage.com/studycombined/groupImage/1575602565910algorith.png","members" : [ ]}`
 ];
 
 const UPDATE_testGroups = [
-  `{"_id":"1", "title": "update", "subtitle": "update", "intro" : "update", "isRecruiting" : true, "category" : [ "프로그래밍", "파이썬"] , "tags" : ["알고리즘 ","파이썬 ","python ","algorithm"],"days" : [0, 4], "leader" : "test1@gmail.com", "startTime" : 17, "min_personnel" : 4, "max_personnel" : 8, "location" : { "lat" : 41.12, "lon" : -50.34 },"endTime" : 19,"thumbnail" : "https://kr.object.ncloudstorage.com/studycombined/groupImage/1575602565910algorith.png","members" : [ ]}`
+  `{"_id":"111", "title": "update", "subtitle": "update", "intro" : "update", "isRecruiting" : true, "category" : [ "프로그래밍", "파이썬"] , "tags" : ["알고리즘 ","파이썬 ","python ","algorithm"],"days" : [0, 4], "leader" : "test1@gmail.com", "startTime" : 17, "min_personnel" : 4, "max_personnel" : 8, "location" : { "lat" : 41.12, "lon" : -50.34 },"endTime" : 19,"thumbnail" : "https://kr.object.ncloudstorage.com/studycombined/groupImage/1575602565910algorith.png","members" : [ ]}`
 ];
 
 const REMOVE_testGroups = [
-  `{"_id":"2", "title": "test", "subtitle": "subtitle2", "intro" : "intro2", "isRecruiting" : true, "category" : [ "프로그래밍", "자바"], "tags" : ["알고리즘 ","java ","자바 ","algorithm"],"days" : [0, 4], "leader" : "test1@gmail.com", "startTime" : 17, "min_personnel" : 4, "max_personnel" : 8, "location" : { "lat" : 41.12, "lon" : -50.34 },"endTime" : 19,"thumbnail" : "https://kr.object.ncloudstorage.com/studycombined/groupImage/1575602565910algorith.png","members" : [ ]}`
+  `{"_id":"211", "title": "test_study", "subtitle": "subtitle2", "intro" : "intro2", "isRecruiting" : true, "category" : [ "프로그래밍", "자바"], "tags" : ["알고리즘 ","java ","자바 ","algorithm"],"days" : [0, 4], "leader" : "test1@gmail.com", "startTime" : 17, "min_personnel" : 4, "max_personnel" : 8, "location" : { "lat" : 41.12, "lon" : -50.34 },"endTime" : 19,"thumbnail" : "https://kr.object.ncloudstorage.com/studycombined/groupImage/1575602565910algorith.png","members" : [ ]}`
 ];
 
 const initializeElastic = async () => {
   await bulkStudyGroups(ADD_testGroups, [], []);
 };
+
 const clearElastic = async () => {
-  await client.delete({ index: "test", id: 1 });
-  await client.delete({ index: "test", id: 2 });
+  await client.delete({ index: "studygroup", id: 111 });
+  await client.delete({ index: "studygroup", id: 211 });
 };
 
 beforeAll(async () => {
@@ -81,71 +82,65 @@ afterAll(async () => {
 /**
  * searchStudyGroup
  */
-test("searchStudyGroup Test", async () => {
+test("특정스터디그룹을_검색한다(searchStudyGroup)", async () => {
   const result = await searchStudyGroup({
-    searchWord: "test",
+    searchWord: "test_study",
     lat: "41.12",
     lon: "-50.34",
+    page: 0,
     isRecruit: true
   });
 
   expect(result).toEqual([testGroups[1], testGroups[0]]);
 });
+
 /**
  * searchStudyGroupWithCategory
  */
-test("searchStudyGroupWithCategory Test", async () => {
+test("카테고리로_스터디그룹을_검색한다(searchStudyGroupWithCategory)", async () => {
   const result = await searchStudyGroupWithCategory({
-    searchWord: "test",
+    searchWord: "test_study",
     category: "파이썬",
     lat: "41.12",
     lon: "-50.34",
+    page: 0,
     isRecruit: true
   });
 
   expect(result).toEqual([testGroups[0]]);
 });
-/**
- * tagStudyGroup
- */
-test("tagStudyGroup Test", async () => {
+
+test("특정_태그를_가진_스터디그룹을_검색한다(tagStudyGroup)", async () => {
   const result = await tagStudyGroup({
-    tags: ["자바"],
+    tags: ["test_study"],
     lat: "41.12",
     lon: "-50.34",
+    page: 0,
     isRecruit: true
   });
 
   expect(result).toEqual([testGroups[1]]);
 });
 
-/**
- * searchAllStudyGroup test
- */
-test("searchAllStudyGroup Test", async () => {
+test("위치내에있는_모든_스터디그룹을_검색한다(searchAllStudyGroup)", async () => {
   const result = await searchAllStudyGroup({
     lat: "41.12",
     lon: "-50.34",
+    page: 0,
     isRecruit: true
   });
 
-  expect(result).toEqual([testGroups[1], testGroups[0]]);
+  expect(result.length).toBeGreaterThan(2);
 });
 
-/**
- * searchAllStudyGroupWithCategory
- */
-test("searchAllStudyGroupWithCategory Test", async () => {
+test("카테고리를_포함하는_모든_그룹을_검색한다(searchAllStudyGroupWithCategory)", async () => {
   const result = await searchAllStudyGroupWithCategory({
-    category: "파이썬",
+    category: "test_group",
     lat: "41.12",
     lon: "-50.34",
+    page: 0,
     isRecruit: true
   });
 
   expect(result).toEqual([testGroups[0]]);
 });
-
-/**
- *
- */
