04a54c178b02f253648dd7e08aac33d7af0f53fd
plm9606 <plm9606@naver.com>
2020-06-17 00:37:17 +0900

    chore: 폰트 추가 및 기본 폰트 변경

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,13 @@ import UserPage from "./pages/users";
 import PartnerPage from "./pages/partners";
 
 const GlobalStyle = createGlobalStyle`
+@font-face { 
+  font-family: 'Recipekorea'; 
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/Recipekorea.woff') format('woff'); 
+  font-weight: normal; 
+  font-style: normal; 
+}
+@font-face { font-family: 'InfinitySans-RegularA1'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@2.1/InfinitySans-RegularA1.woff') format('woff'); font-weight: normal; font-style: normal; }
   @import "bulmaCarousel.sass";
   @import url("https://fonts.googleapis.com/earlyaccess/nanumgothic.css");
   @import url('https://fonts.googleapis.com/css?family=Black+Han+Sans&display=swap');
