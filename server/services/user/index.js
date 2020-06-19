531fb7e4c9b56993da30108f7812e35a0530def5
plm9606 <plm9606@naver.com>
2020-06-20 03:21:10 +0900

    feat: reservation 서비스와 연결

--- server/services/user/index.js
+++ server/services/user/index.js
@@ -7,3 +7,40 @@ const User = require("./User");
 const user = new User(USER_NAME, USER_HOST, USER_PORT);
 
 user.connectToAppListManager();
+
+let tryConnect = setInterval(() => {
+  connectToService.bind(user)("reservation");
+}, 1000);
+
+async function connectToService(name) {
+  try {
+    let isServiceConnected = false;
+    const service = await this.connectToApp(
+      name,
+      () => {
+        isServiceConnected = true;
+        console.log(`${name} service connect`);
+      },
+      () => {},
+      () => {
+        isServiceConnected = false;
+        console.log(`${name} service end`);
+      },
+      () => {
+        isServiceConnected = false;
+        console.log(`${name} service error`);
+      }
+    );
+
+    setInterval(() => {
+      if (!isServiceConnected) {
+        console.log(`try connect to ${name}`);
+        service.connect();
+      }
+    }, 2000);
+
+    clearInterval(tryConnect);
+  } catch (e) {
+    console.log(e);
+  }
+}
