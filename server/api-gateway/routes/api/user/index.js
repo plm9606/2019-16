605f0c74993d60d6ac1a914f75106f4274e5ad87
plm9606 <plm9606@naver.com>
2020-06-20 03:11:41 +0900

    feat: 유저의 스터디 참여기록 조회 api 생성

--- server/api-gateway/routes/api/user/index.js
+++ server/api-gateway/routes/api/user/index.js
@@ -4,37 +4,54 @@ const router = express.Router();
 const { makePacket } = require("../../../../lib/tcp/util");
 
 module.exports = function (apiGateway) {
-    router.post("/joinGroups", (req, res, next) => {
-        const joiningGroups = req.body;
+  router.post("/joinGroups", (req, res, next) => {
+    const joiningGroups = req.body;
 
-        req.packet = makePacket(
-            "GET",
-            "apigateway",
-            "updateJoiningGroups",
-            "updateJoiningGroups",
-            { ...joiningGroups },
-            {},
-            req.resKey,
-            apiGateway.context
-        );
+    req.packet = makePacket(
+      "GET",
+      "apigateway",
+      "updateJoiningGroups",
+      "updateJoiningGroups",
+      { ...joiningGroups },
+      {},
+      req.resKey,
+      apiGateway.context
+    );
 
-        next();
-    });
-    router.post("/ownGroups", (req, res, next) => {
-        const ownGroups = req.body;
+    next();
+  });
+  router.post("/ownGroups", (req, res, next) => {
+    const ownGroups = req.body;
 
-        req.packet = makePacket(
-            "GET",
-            "apigateway",
-            "updateOwnGroups",
-            "updateOwnGroups",
-            { ...ownGroups },
-            {},
-            req.resKey,
-            apiGateway.context
-        );
+    req.packet = makePacket(
+      "GET",
+      "apigateway",
+      "updateOwnGroups",
+      "updateOwnGroups",
+      { ...ownGroups },
+      {},
+      req.resKey,
+      apiGateway.context
+    );
 
-        next();
-    });
-    return router;
+    next();
+  });
+
+  router.get("/history/:userId", (req, res, next) => {
+    const { userId } = req.params;
+
+    req.packet = makePacket(
+      "GET",
+      "apigateway",
+      "getUserHistoryAll",
+      "getHistoriesByIds",
+      { userId },
+      {},
+      req.resKey,
+      apiGateway.context
+    );
+
+    next();
+  });
+  return router;
 };
