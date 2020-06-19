40759a5401f63b0da3cedab87167c5621e8deabe
plm9606 <plm9606@naver.com>
2020-06-20 03:28:01 +0900

    fix: 간단한 배포 스크립트 수정

--- server/startServices_deploy_ver.sh
+++ server/startServices_deploy_ver.sh
@@ -1,4 +1,5 @@
 # ./server/ 로 이동 후 실행해야한다. 
+# 해당 스크립트 디렉토리에 env파일이 있어야 한다. 
 # package.json을 가지고 있는 디렉토리를 찾고 해당 디렉토리에서
 # npm update를 실행한 후에 백그라운드로 모듈을 실행한다. 
 
@@ -7,15 +8,15 @@ for node in $NODE_LIST
 do     
 	echo find ${node%package.json}     
 	path=${node%package.json}index.js
-	npm update --prefix ${node%package.json} 
+	# npm update --prefix ${node%package.json} 
 	
 	if [ -f $path ]; then
 		echo index.js파일이 존재함
-		splitedArr=($(echo ${node%package.json} | tr "/" "\n"))
-		length=${#arr[*]}
-		echo arr is $arr length is $length last item is ${arr[$length - 1]}
 #		nodemon $path &
-		name=${splitedArr[$length - 1]}
+		IFS=/ read -ra ary <<<"${node%package.json}"
+
+		name=${ary[-1]}
+		echo name is ${name}
 		pm2 start $path --name $name
 	fi
 done
